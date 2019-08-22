570ded3ddf51a86aaf64b049f9af0070a3053d45
Hongxiong Xian (Wicresoft North America Ltd) <v-hoxian@microsoft.com>
2019-08-22 01:24:46 -0700

    Refine OperateAzureBlob_SASToken_REST.ps1

--- REST_PS/OperateAzureBlob_SASToken_REST.ps1
+++ REST_PS/OperateAzureBlob_SASToken_REST.ps1
@@ -1,5 +1,57 @@
 
 
+<#
+How to run this script:
+
+Step 1: Download the script to local, e.g, D:, then change directory to D
+Step 2: Initialize the below parameters
+
+    $storageAccountName = "YourStorageAccountName"
+    $containerName = "YourContainerName"
+    $targetFolderPath = "WhereFilesAreSaved" # Default valure is current directory if it's null
+    $SASToken = "YourSASToken"
+
+Step 3: Run the script. The below are some examples.
+
+# Example 1: List the blobs and download all of them
+./OperateAzureBlob_SASToken_REST.ps1 -Download "all" -storageAccount $storageAccountName -containerName $containerName `
+    -SASToken $SASToken -targetFolderPath $targetFolderPath -List
+
+# Example 2: Download the specified file(s), multiple files split with comma
+./OperateAzureBlob_SASToken_REST.ps1 -Download "linux.vhd,freebsd.vhd" -storageAccount $storageAccountName `
+    -containerName $containerName -SASToken $SASToken -targetFolderPath $targetFolderPath
+
+# Example 3: List the blobs and delete all of them
+./OperateAzureBlob_SASToken_REST.ps1 -Delete "all" -storageAccount $storageAccountName -containerName $containerName `
+    -SASToken $SASToken -targetFolderPath $targetFolderPath -List
+
+# Example 4: Delete the specified file(s), multiple files split with comma
+./OperateAzureBlob_SASToken_REST.ps1 -Delete "linux.vhd,freebsd.vhd" -storageAccount $storageAccountName `
+    -containerName $containerName -SASToken $SASToken
+
+# Example 5: List the blobs
+./OperateAzureBlob_SASToken_REST.ps1 -storageAccount $storageAccountName -containerName $containerName `
+    -SASToken $SASToken -targetFolderPath $targetFolderPath -List
+
+#>
+
+param (
+    [string]$Download,
+    [string]$Delete,
+    [switch] $List,
+
+    [Parameter(Mandatory)]
+    [string]$storageAccount,
+    
+    [Parameter(Mandatory)]
+    [string]$containerName,
+
+    [Parameter(Mandatory)]
+    [string]$SASToken,
+
+    [string]$targetFolderPath
+)
+
 function Run-InvokeRestMethod {
     param (
         [string]$blobName,
@@ -80,16 +132,28 @@ function Download-BlobFromAzure {
     $result = List-BlobFromAzure -storageAccountName $storageAccountName `
         -SASToken $SASToken -containerName $containerName
 
-    if ($result.EnumerationResults.Blobs.Blob | ? {$_.name -eq $blobName}) {
-        "Find the $blobName and start to download it from $storageAccountName"
+    if ($blobName -eq "all") {
+        foreach ($blob in $result.EnumerationResults.Blobs.Blob.Name) {
+            $url = "https://$($StorageAccountName).blob.core.windows.net/$($ContainerName)/$($blob)$SASToken"
+            Run-InvokeRestMethod -blobName $blob -httpAction "GET" -targetFolderPath $targetFolderPath -url $url
+            "Download $blob successfully"
+        }
     } else {
-        "Failed to download the $blobName on $storageAccountName because it doesn't exist"
-        return "FAIL"
-    }
+        foreach ($blob in $blobName.split(",")) {
+            if ($result.EnumerationResults.Blobs.Blob | ? {$_.name -eq $blob}) {
+            "Find the $blob and start to download it from $storageAccountName"
+            } else {
+                "Failed to download the $blob on $storageAccountName because it doesn't exist"
+                continue
+            }
 
-    $url = "https://$($StorageAccountName).blob.core.windows.net/$($ContainerName)/$($BlobName)$SASToken"
-    Run-InvokeRestMethod -blobName $BlobName -httpAction "GET" -targetFolderPath $targetFolderPath -url $url
+            $url = "https://$($StorageAccountName).blob.core.windows.net/$($ContainerName)/$($blob)$SASToken"
+            Run-InvokeRestMethod -blobName $blob -httpAction "GET" -targetFolderPath $targetFolderPath -url $url
+            "Download $blob successfully"
+        }
+    }
 }
+
 
 # Delete the specified blob file in a container
 function Delete-BlobFromAzure {
@@ -110,35 +174,59 @@ function Delete-BlobFromAzure {
     $result = List-BlobFromAzure -storageAccountName $storageAccountName `
         -SASToken $SASToken -containerName $containerName
 
-    if ($result.EnumerationResults.Blobs.Blob | ? {$_.name -eq $blobName}) {
-        "Find the $blobName and start to delete it from $storageAccountName"
+    if ($blobName -eq "all") {
+        foreach ($blob in $result.EnumerationResults.Blobs.Blob.Name) {
+            $url = "https://$($StorageAccountName).blob.core.windows.net/$($ContainerName)/$($blob)$SASToken"
+            Run-InvokeRestMethod -blobName $blob -httpAction "DELETE" -url $url
+            "Delete $blob successfully"
+        }
     } else {
-        "Failed to delete the $blobName on $storageAccountName because it doesn't exist"
-        return "FAIL"
+        foreach ($blob in $blobName.split(",")) {
+            if ($result.EnumerationResults.Blobs.Blob | ? {$_.name -eq $blob}) {
+            "Find the $blob and start to delete it from $storageAccountName"
+            } else {
+                "Failed to delete the $blob on $storageAccountName because it doesn't exist"
+                continue
+            }
+
+            $url = "https://$($StorageAccountName).blob.core.windows.net/$($ContainerName)/$($blob)$SASToken"
+            Run-InvokeRestMethod -blobName $blob -httpAction "DELETE" -url $url
+            "Delete $blob successfully"
+        }
     }
-
-    $url = "https://$($StorageAccountName).blob.core.windows.net/$($ContainerName)/$($BlobName)$SASToken"
-    Run-InvokeRestMethod -blobName $BlobName -httpAction "DELETE" -url $url
 }
 
-#############################################################################################
+####################################### Main ######################################################
 
-$storageAccount = "YourStorageAccountName"
-$containerName = "YourContainerName"
-$targetFolderPath = "YourFolder"
-$SASToken = "YourSASToke"
+if (-not $targetFolderPath) {
+    $targetFolderPath = pwd
+} 
 
-$result = List-BlobFromAzure -StorageAccountName $storageAccount -SASToken $SASToken -containerName $containerName
-Write-OutPut "The blobs in ${containerName}:"
-foreach ($item in $result.EnumerationResults.Blobs.Blob.Name) {
-    Write-OutPut "    $item"
+if (!(Test-Path $targetFolderPath)) {
+    New-Item -Path $targetFolderPath -ItemType Directory -Force | Out-Null
 }
-Write-OutPut "`n"
 
-$BlobName = "BlobFileToBeDownloaded"
-Download-BlobFromAzure -StorageAccountName $storageAccount -containerName $containerName `
-    -blobName $BlobName -targetFolderPath $targetFolderPath -SASToken $SASToken
+Write-OutPut "Storage account name: $storageAccount"
+Write-OutPut "Container name: $containerName"
+Write-OutPut "The target path: $targetFolderPath`n"
 
-$BlobName = "BlobFileToBeDeleted"
-Delete-BlobFromAzure -StorageAccountName $storageAccount -containerName $containerName `
-    -blobName $BlobName -SASToken $SASToken
+
+if ($List) {
+    $result = List-BlobFromAzure -StorageAccountName $storageAccount -SASToken $SASToken -containerName $containerName
+    Write-OutPut "The blobs in ${containerName}:"
+    foreach ($item in $result.EnumerationResults.Blobs.Blob.Name) {
+        Write-OutPut "    $item"
+    }
+    Write-OutPut "`n"
+}
+
+if ($Download) {
+    Download-BlobFromAzure -StorageAccountName $storageAccount -containerName $containerName `
+        -blobName $Download -targetFolderPath $targetFolderPath -SASToken $SASToken
+}
+
+if ($Delete) {
+    Delete-BlobFromAzure -StorageAccountName $storageAccount -containerName $containerName `
+        -blobName $Delete -SASToken $SASToken
+}
+
